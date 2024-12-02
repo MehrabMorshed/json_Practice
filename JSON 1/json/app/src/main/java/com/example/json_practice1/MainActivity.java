935defbbd76d1afe09711845bc659f31d994873d
Mehrab <136856380+MehrabMorshed@users.noreply.github.com>
2024-12-02 18:26:35 +0600

    Add files via upload

---
+++ JSON 1/json/app/src/main/java/com/example/json_practice1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.json_practice1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView display;
+    private String json_url=
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
